f9b514ba6d6506d5a77b0af9d57f075f511a7909
Dominic <dominicprestia0@gmail.com>
2023-06-30 17:45:13 -0500

    added end game and start game states

--- index.js
+++ index.js
@@ -8,12 +8,12 @@
 // }, false);
 
 //This helps me determine which ouse button is being clicked
-document.addEventListener('mousedown', (click) => {
-  var clickName = click.button
+// document.addEventListener('mousedown', (click) => {
+//   var clickName = click.button
 
 
-  console.log(`Key pressed ${clickName} \n`);
-}, false);
+//   console.log(`Key pressed ${clickName} \n`);
+// }, false);
 
 let paddle = document.querySelector('.paddle')
 let paddle_X = window.getComputedStyle(paddle, null).getPropertyValue("left")
@@ -45,41 +45,63 @@ let paddleID = null;
 let paddleID2 = null;
 let keypressed = false;
 let timer = 0;
+let timer_id = null;
+let prompt = document.querySelector('.gameprompt');
+let gameprompt_ID = null;
+let timeMilliseconds = document.querySelector('.milliseconds')
+let timeSeconds = document.querySelector('.seconds')
+let milliSeconds = 0;
+let seconds = 0;
+let gameOver = false;
 
-console.log("Paddle X Position: " + paddle_Coord)
-console.log("Paddle Y Position: " + paddle_Coord_Y)
-console.log("Ball Y Position: " + y_coord)
-console.log("Ball X Position: " + x_coord)
+// console.log("Paddle X Position: " + paddle_Coord)
+// console.log("Paddle Y Position: " + paddle_Coord_Y)
+// console.log("Ball Y Position: " + y_coord)
+// console.log("Ball X Position: " + x_coord)
+// console.log(gameboard_pos)
 
-function pad2(event) {
-  return event.code
-}
+
 
 document.addEventListener("keyup", startGame)
+
+gamePrompt();
+
+
 
 function startGame(event) {
 
   //if enter key is pressed start the game
   if (event.code === "Enter" && inProgress == false) {
     inProgress = true;
+    timer_id = setInterval(timerFunc, 100)
+    clearInterval(gameprompt_ID)
+    prompt.style.display = "none";
     id = setInterval(frame, speed);
     console.log("start game")
   }
 
+  document.addEventListener("keyup", (event) => {
+    if (event.code === "KeyR") {
+      location.reload();
+    }
+  })
+
   document.addEventListener("keyup", function (event) {
-    if (event.code === "ArrowLeft") {
+    if (event.code === "ArrowLeft" && gameOver == false) {
       clearInterval(paddleID)
       clearInterval(paddleID2)
       paddleID = null
       paddleDir = -1
+      paddlespeed = initialPaddleSpeed
       if (paddleID == null)
         paddleID = setInterval(paddleMovement, speed);
     }
-    else if (event.code === "ArrowRight") {
+    else if (event.code === "ArrowRight" && gameOver == false) {
       clearInterval(paddleID2)
       clearInterval(paddleID)
       paddleID = null
       paddleDir = 1
+      paddlespeed = initialPaddleSpeed
       if (paddleID == null)
         paddleID2 = setInterval(paddleMovement, speed)
     }
@@ -87,20 +109,20 @@ function startGame(event) {
 
 
   function frame() {
-
-
-
-
-
+    console.log("I'm in here")
 
     document.addEventListener("keydown", function (event) {
       if (event.code === "KeyR") {
         clearInterval(id)
+        clearInterval(timer_id)
+        timeSeconds.innerHTML = 0;
+        timeMilliseconds.innerHTML = 0;
         inProgress = false;
         y_coord = parseInt(ball_Y)
         x_coord = parseInt(ball_X)
         ball.style.top = parseInt(y_coord) + "px";
         ball.style.left = parseInt(x_coord) + "px";
+        gamePrompt();
         console.log("Reset")
       }
     })
@@ -117,6 +139,13 @@ function startGame(event) {
       hitBorder = setInterval(topBorderHit, 200)
     }
     if (y_coord + 10 > gameboard_pos.height) {
+      clearInterval(id)
+      clearInterval(paddleID)
+      clearInterval(paddleID2)
+      gameboard.style.borderBottomColor = "red"
+      hitBorder = setInterval(topBorderHit, 200)
+      gameOver = true;
+      gamePrompt();
       y_dir *= -1;
     }
     if (x_coord + 10 > gameboard_pos.width) {
@@ -133,9 +162,8 @@ function startGame(event) {
       hitBorder = setInterval(topBorderHit, 200)
     }
 
-    if (y_coord > paddle_Coord_Y -15 && x_coord + 5 < paddle_Coord + 25 && x_coord -5 > paddle_Coord -25){
-      y_dir *=-1
-      console.log("HIT")
+    if (y_coord - 10 < paddle_Coord_Y + 5 && y_coord + 10 > paddle_Coord_Y - 5 && x_coord + 5 < paddle_Coord + 25 && x_coord - 5 > paddle_Coord - 25) {
+      y_dir *= -1
     }
     y_coord = y_coord + 1 * y_dir * ballspeed;
     x_coord = x_coord + 1 * x_dir * ballspeed;
@@ -147,28 +175,39 @@ function startGame(event) {
 
 }
 
+function timerFunc() {
+  milliSeconds++
+  if (milliSeconds == 10) {
+    milliSeconds = 0
+    seconds++
+    timeSeconds.innerHTML = seconds
+    if (parseInt(timeSeconds.innerHTML) % 5 == 0) {
+      ballspeed = ballspeed + .2
+      initialPaddleSpeed = initialPaddleSpeed + .3
+    }
+  }
+}
+
+
 function paddleMovement() {
 
   if (paddle_Coord + 25 > gameboard_pos.width - 4) {
     console.log("Stuck TOP")
     paddlespeed = 0;
-    paddle_Coord -= 5;
+    paddle_Coord = 370;
     clearInterval(paddleID2)
   }
   if (paddle_Coord - 25 < 0 && paddlespeed > 0) {
     paddlespeed = 0;
-    paddle_Coord += 5;
+    paddle_Coord = 30;
     console.log("Stuck bottom")
     clearInterval(paddleID)
-    // console.log(paddleID)
   }
-  else {
-    paddlespeed = initialPaddleSpeed
-  }
-
-
   paddle_Coord = paddle_Coord + 1 * paddleDir * paddlespeed;
   paddle.style.left = paddle_Coord + 'px'
+
+
+
 
 }
 
@@ -185,5 +224,23 @@ function getRandomAngle(min, max) {
 function topBorderHit() {
   gameboard.style.borderTopColor = "green"
   clearInterval(hitBorder)
+}
+
+function gamePrompt() {
+  gameprompt_ID = setInterval(showPrompt, 500)
+}
+
+function showPrompt() {
+
+  if (prompt.style.display === "none") {
+    prompt.style.display = "block";
+  } else {
+    prompt.style.display = "none";
+  }
+
+  if (gameOver == true) {
+    prompt.innerHTML = "Press 'R' to try again?"
+  }
+
 }
 
